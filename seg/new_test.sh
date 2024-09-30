4c8798cea195337a50bfdc097bcaa53d42fb7440
tanluuuuuuu <ngovantanluu@gmail.com>
2024-09-30 13:42:26 +0700

    update

---
+++ seg/new_test.sh
@@ -0,0 +1,22 @@
+# Obtained from: https://github.com/lhoyer/DAFormer
+# Modifications: Add HR, LR, and attention visualization
+# ---------------------------------------------------------------
+# Copyright (c) 2021-2022 ETH Zurich, Lukas Hoyer. All rights reserved.
+# Licensed under the Apache License, Version 2.0
+# ---------------------------------------------------------------
+
+#!/bin/bash
+
+CONFIG_FILE="configs/config_mic_v5_dynamic.py"  # or .json for old configs
+CHECKPOINT_FILE="/kaggle/input/dynamic-iter-19k/pytorch/default/1/iter_19000.pth"
+SHOW_DIR="preds"
+echo 'Config File:' $CONFIG_FILE
+echo 'Checkpoint File:' $CHECKPOINT_FILE
+echo 'Predictions Output Directory:' $SHOW_DIR
+python -m tools.test ${CONFIG_FILE} ${CHECKPOINT_FILE} --eval mIoU --show-dir ${SHOW_DIR} --opacity 1
+
+# Uncomment the following lines to visualize the LR predictions,
+# HR predictions, or scale attentions of HRDA:
+#python -m tools.test ${CONFIG_FILE} ${CHECKPOINT_FILE} --show-dir ${SHOW_DIR}_LR --opacity 1 --hrda-out LR
+#python -m tools.test ${CONFIG_FILE} ${CHECKPOINT_FILE} --show-dir ${SHOW_DIR}_HR --opacity 1 --hrda-out HR
+#python -m tools.test ${CONFIG_FILE} ${CHECKPOINT_FILE} --show-dir ${SHOW_DIR}_ATT --opacity 1 --hrda-out ATT
