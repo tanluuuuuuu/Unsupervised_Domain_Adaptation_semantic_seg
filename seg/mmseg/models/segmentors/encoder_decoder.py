24defed0cd2efb9e1cf85b8e33c5b153ae2a0985
tanluuuuuuu <ngovantanluu@gmail.com>
2024-09-30 15:38:48 +0700

    update

--- seg/mmseg/models/segmentors/encoder_decoder.py
+++ seg/mmseg/models/segmentors/encoder_decoder.py
@@ -362,7 +362,8 @@ class EncoderDecoder(BaseSegmentor):
                 size = img.shape[2:]
             else:
                 size = img_meta[0]['ori_shape'][:2]
-            print("IMG SIZE: ", size)
+            print("IMG META SIZE: ", size)
+            print("IMG SIZE: ", img.shape[2:])
             seg_logit = resize(
                 seg_logit,
                 size=size,
